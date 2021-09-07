aa798ee62a29d64deab545670646e8abbca8add5
unknown <devdaulton2@gmail.com>
2021-09-07 18:09:32 -0400

    client basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css';
+// import React, {useState, useEffect} from 'react';
+import Sitebar from './Components/Navbar';
+
+// const [sessionToken, setSessionToken] = useState('');
+
+// useEffect(() => {
+//   if (localStorage.getItem('token')) {
+//     setSessionToken(localStorage.getItem('token'));
+//   }
+// }, [])
+
+// const updateToken = (newToken) => {
+//   localStorage.setItem('token', newToken);
+//   setSessionToken(newToken);
+//   console.log(sessionToken);
+// }
 
 function App() {
   return (
     <div>
-      "BirdR Aryn"
+      <Sitebar />
     </div>
   );
 }
