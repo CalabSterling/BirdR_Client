117f39196a3b169d97e78c15e3b7ab0098efa80e
Kara Smith <MsKaraBSmith@gmail.com>
2021-09-07 18:15:04 -0400

    beginning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Sitebar from './home/Navbar';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+
+  useEffect(() => {
+    document.title = "BirdR"
+  }, []);
+
   return (
     <div>
-      "BirdR kara"
+      <Sitebar/>
     </div>
   );
 }
