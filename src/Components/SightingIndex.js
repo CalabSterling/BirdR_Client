60bb143dabc09e6c892b84d48de9728d9ed875a8
unknown <devdaulton2@gmail.com>
2021-09-09 18:47:49 -0400

    SightingIndex

---
+++ src/Components/SightingIndex.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import SightingCreate from './SightingCreate';
+import SightingTable from './SightingTable';
+import SightingEdit from './SightingEdit';
+
+const SightingIndex = (props) => {
+    const [sightings, setSightings] = useState([]);
+    const [updateActive, setUpdateActive] = useState(false);
+    const [sightingToUpdate, setSightingToUpdate] = useState({});
+
+    const fetchSightings = () => {
+        fetch('http://localhost:3000/sighting' {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
+        }) .then( (res) => res.json())
+        .then((sightingData) => {
+            setSightings(sightingData)
+            console.log(sightingData);
+        })
+    }
+
+    const editUpdateSighting = (sighting) => {
+        setSightingToUpdate(sighting);
+        console.log(workout);
+    }
+
+    const updateOn = () => {
+        setUpdateActive(true);
+    }
+
+    const updateOff = () => {
+        setUpdateActive(false);
+    }
+
+    useEffect(() => {
+        fetchSightings();
+    }, [])
+
+    return(
+        <Container>
+            <Row>
+                <Col md="3">
+                    <SightingCreate fetchSightings={fetchSightings} token={props.token}/>
+                </Col>
+                <Col md="9">
+                    <SightingTable sightings={sightings} editUpdateSighting={editUpdateSighting} updateOn={updateOn} fetchSightings={fetchSightings} token={props.token} />
+                </Col>
+                {updateActive ? <SightingEdit sightingToUpdate={sightingToUpdate} updateOff={updateOff} token={props.token} fetchSightings={fetchSightings} /> : <></>}
+            </Row>
+        </Container>
+    )
+}
+
+export default SightingIndex;
